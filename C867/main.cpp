c46b8ddd2049e64e8124dca4f214468fb7c0f89a
Jonathan Bartolo <jonathan.e.bartolo@gmail.com>
2024-12-13 18:30:35 -0800

    Initial Commit

---
+++ C867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867
+//
+//  Created by Jon on 12/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
